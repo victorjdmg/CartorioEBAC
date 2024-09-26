8724c3b4e68cea1c7275b0e72846d590f3ffbdfd
victorjdmg <victor.jdmg@hotmail.com>
2024-09-26 18:27:01 -0300

    Opção de saída.
    Opção de saída adicionado no código.

--- Cartório da EBAC.c.cpp
+++ Cartório da EBAC.c.cpp
@@ -127,7 +127,8 @@ int main()
 		printf("Escolha a opção desejada do menu:\n\n");
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - Consultar nomes\n");
-		printf("\t3 - Deletar nomes\n\n");	
+		printf("\t3 - Deletar nomes\n");
+		printf("\t4 - Sair do sistema\n\n");
 		printf("Opcão:	"); //fim do menu
 	
 		scanf("%d", &opcao); //armazenando a escolha do usuário
@@ -147,6 +148,11 @@ int main()
 			
 			case 3:
 			deletar();
+			break;
+			
+			case 4:
+			printf("Obrigado por utilizar o sistema");
+			return 0;
 			break;
 									
 			default:
